e12caafdbfb190b3891d8778d4bccefd13d5ea28
CuongNM <cuongnmd00335@gmail.com>
2018-02-23 10:36:22 +0700

    add http

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router  = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const createMiddleware = require('../http/middlewares/create.middleware');
+const crudController   = require('../http/controllers/crud.controller');
+
+// Create
+// data => middleware => controller => service
+router.post('/create',
+    createMiddleware,
+    crudController.create
+);
+
+// Find All
+router.get('/all', crudController.findAll);
+
+// Find One
+router.get('/detail/:id');
+
+// Update
+router.put('/');
+
+// Delete
+router.delete('/');
 
 module.exports = router;
